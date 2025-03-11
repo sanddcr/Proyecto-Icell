62edf4fabcf4fc92f289f087c2953edcf10faac4
Alexis <josesandoya@icloud.com>
2025-03-11 10:11:39 -0600

    proyecto final

---
+++ src/main/java/com/Icell/IcellApplication.java
@@ -0,0 +1,13 @@
+package com.Icell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcellApplication.class, args);
+	}
+
+}
